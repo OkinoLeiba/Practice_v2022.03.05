c16a2bf7a1e3f5e9ca509905ee6fa76bf31200de
Okino Kamali Leiba <17099286+OkinoLeiba@users.noreply.github.com>
2022-08-03 19:27:44 -0400

    Delete PASSREADACCESS.h

--- PASSREADACCESS.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <cstdio>
-
-//memory safety: prevent buffer read overflow 
-template <size_t index, typename pass_len, size_t length>
-pass_len& get(pass_len (&arr)[length]) {static_assert(index < length, "Access Denied"); return arr[length]; }
- 
